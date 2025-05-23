261978617f838603cd85297c1505d586cf02cebd
Fernanda Cristina Skoula <fernandacristina.skoula@philips.com>
2025-05-23 15:57:58 -0300

    feat: adicionando lista de compras [fcskoula]

--- components/ButtonDeleteComponente.js
+++ components/ButtonDeleteComponente.js
@@ -1,7 +1,11 @@
 export const ButtonDeleteComponent = {
   render: (props = {}) => {
+    setTimeout(() => {
+      const el = document.getElementById(props.id);
+      if (el) el.addEventListener("click", () => { props.funcao(props.props)});
+    }, 0);
     return `
-      <button class="btn-trash" onclick="()=>{alert("Você excluiu")}">
+      <button class="btn-trash" id="${props.id}">
         <img src="assets/trash.svg" alt="Excluir">
       </button>
     `;
--- components/NovaCompraComponent.js
+++ components/NovaCompraComponent.js
@@ -1,79 +1,96 @@
 import { ButtonComponent } from "./ButtonComponent.js";
 import { ButtonDeleteComponent } from "./ButtonDeleteComponente.js";
 import { checkBoxComponent, InputComponent } from "./InputComponent.js";
-import { TableComponent } from "./TableComponent.js";
+import { ListaCompraComponent } from "./ListaCompraComponent.js";
 
 export const NovaCompraComponent = {
     render: () => {
-        const list = TableComponent.render(
-            {
-                id: 1,
-                items: [
-                    {
-                        Forncedor: "Fornecedor",
-                        Produto: "Produto",
-                        quantidade: "Quantidade",
-                        acao: " ",
-                    },
-                    {
-                        Forncedor: "Fornecedor A",
-                        Produto: "Produto A",
-                        quantidade: "96 Caixas",
-                        acao: ButtonDeleteComponent.render(),
-                    },
-                    {
-                        Forncedor: "Fornecedor A",
-                        Produto: "Produto B",
-                        quantidade: "24 Caixas",
-                        acao: ButtonDeleteComponent.render(),
-                    },
-                    {
-                        Forncedor: "Fornecedor A",
-                        Produto: "Produto C",
-                        quantidade: "69 Caixas",
-                        acao: ButtonDeleteComponent.render(),
-                    },
-                    {
-                        Forncedor: "Fornecedor A",
-                        Produto: "Produto D",
-                        quantidade: "11 Caixas",
-                        acao: ButtonDeleteComponent.render(),
-                    },
-                ]
+        let detalhes = [{ id: 0, produto: "", quantidade: 0 }]
+
+        function onAddProduct() {
+            const novoDetalhe = [
+                ...detalhes,
+                { id: detalhes.length, produto: "", quantidade: 0 }
+            ]
+
+            detalhes = novoDetalhe;
+            console.log(detalhes)
+        }
+
+        function onRemoveProduct(id) {
+            let index = 0;
+            const detalheAtualizado = [];
+            for (const detalhe of detalhes) {
+                if (detalhe.id != id) {
+                    detalheAtualizado.push({ id: index, produto: detalhe.detalhes })
+                    index ++
+                } 
             }
-        )
+
+            detalhes = detalheAtualizado;
+            console.log(detalhes)
+        }
+
+        const list = {colunas: ["Produto", "Quantidade"],items: detalhes}
         return `
-      <section>
+            <div class="table-component">
+                <table>
+                    <tr>
+                        ${list.colunas.map((content) => {
+                            return `
+                                <th>${content}</th>
+                            `;
+                        }).join('')}
+                    </tr>
+                    ${list.items.map((content) => {
+                        return `
+                            <tr>
+                                ${Object.keys(content).map((key) => {
+                                    if (key == "produto")
+                                        return `<td>
+                                                    <input type="text" value="${content.produto}"
+                                                </td>
+                                        `
 
-        <div class="inputFornecedor">
-            ${InputComponent.render({ type: "text", placeholder: "Fornecedor:" })}
-        </div>
-
-        <div class="inputProduto">
-            ${InputComponent.render({ type: "text", placeholder: "Produto:" })}
-        </div>
-
-        <div class="containerCompra">
-            <div class="inputQuantidade">
-                ${InputComponent.render({ type: "number", placeholder: "Quantidade:" })}
+                                }).join('')}
+                                ${ButtonDeleteComponent.render({ id:"delete-button", label: "remover", funcao: onRemoveProduct})}
+                            </tr>
+                        `;
+                    }).join('')}
+                </table>
             </div>
-            <div class="checkBox">
-                ${checkBoxComponent.render({ type: "checkbox" })}
-            </div>
-        </div>
-
-        <div class="btAdicionar">
-            ${ButtonComponent.render({ label: "Adicionar" })}
-        </div>
-
-        <div class="corpo">
-            ${list}
-        </div>
-        
-        <div class="btConfirmar">
-            ${ButtonComponent.render({ label: "Confirmar Compra" })}
-        </div>
-      </section>
-    `;
+            ${ButtonComponent.render({ id:"add-button", label: "Adicionar", funcao: onAddProduct})}
+        `;
     }
 };
+        // <section>
+  
+        //   <div class="inputFornecedor">
+        //       ${InputComponent.render({ type: "text", placeholder: "Fornecedor:" })}
+        //   </div>
+  
+        //   <div class="inputProduto">
+        //       ${InputComponent.render({ type: "text", placeholder: "Produto:" })}
+        //   </div>
+  
+        //   <div class="containerCompra">
+        //       <div class="inputQuantidade">
+        //           ${InputComponent.render({ type: "number", placeholder: "Quantidade:" })}
+        //       </div>
+        //       <div class="checkBox">
+        //           ${checkBoxComponent.render({ type: "checkbox" })}
+        //       </div>
+        //   </div>
+  
+        //   <div class="btAdicionar">
+            //   ${ButtonComponent.render({ label: "Adicionar" })}
+        //   </div>
+  
+        //   <div class="corpo">
+        //       ${list}
+        //   </div>
+          
+        //   <div class="btConfirmar">
+        //       ${ButtonComponent.render({ label: "Confirmar Compra" })}
+        //   </div>
+        // </section>
